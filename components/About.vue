4bce43235e9cff377759d543e8c651ab8d404fc9
Renato Filipi <re.rosa98@gmail.com>
2022-05-11 23:10:15 -0300

    wip about

---
+++ components/About.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="block">
+    <div class="flex flex-col m-12 lg:m-24">
+      <h2 class="font-qs text-gray-800 font-bold text-5xl mb-16">About</h2>
+      <div class="flex flex-col gap-12 divide-y-2">
+        <div>
+          <h3 class="font-qs text-gray-800 font-bold text-3xl mb-8">
+            Text Formatter
+          </h3>
+          <div class="flex flex-col gap-3">
+            <p class="font-qs text-gray-800 font-medium text-lg">
+              This Text Formatter is a online text formatting tool that can
+              format the given text. Currently suports: Uppercase all the given
+              text, Lowercase all the given text, Capitalize the sentence and
+              Capitalize the words from the given text.
+            </p>
+          </div>
+        </div>
+        <div class="pt-12 flex gap-3 flex-col">
+          <h3 class="font-qs text-gray-800 font-bold text-3xl mb-8">
+            Examples
+          </h3>
+          <div class="flex gap-5 flex-wrap">
+            <div class="bg-white overflow-hidden border border-gray-200 p-3">
+              <div class="m-2 text-justify text-sm font-qs">
+                <h2 class="font-bold text-lg h-2 mb-8">Uppercase all</h2>
+                <p class="text-sm">
+                  LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND
+                  TYPESETTING INDUSTRY. LOREM IPSUM HAS BEEN THE INDUSTRY'S
+                  STANDARD DUMMY TEXT EVER SINCE THE 1500S, WHEN AN UNKNOWN
+                  PRINTER TOOK A GALLEY OF TYPE AND SCRAMBLED IT TO MAKE A TYPE
+                  SPECIMEN BOOK. IT HAS SURVIVED NOT ONLY FIVE CENTURIES, BUT
+                  ALSO THE LEAP INTO ELECTRONIC TYPESETTING, REMAINING
+                  ESSENTIALLY UNCHANGED. IT WAS POPULARISED IN THE 1960S WITH
+                  THE RELEASE OF LETRASET SHEETS CONTAINING LOREM IPSUM
+                  PASSAGES, AND MORE RECENTLY WITH DESKTOP PUBLISHING SOFTWARE
+                  LIKE ALDUS PAGEMAKER INCLUDING VERSIONS OF LOREM IPSUM.
+                </p>
+              </div>
+            </div>
+            <div class="bg-white overflow-hidden border border-gray-200 p-3">
+              <div class="m-2 text-justify text-sm font-qs">
+                <h2 class="font-bold text-lg h-2 mb-8">Lowercase all</h2>
+                <p class="text-sm">
+                  lorem ipsum is simply dummy text of the printing and
+                  typesetting industry. lorem ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. it has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. it was popularised in the 1960s with
+                  the release of letraset sheets containing lorem ipsum
+                  passages, and more recently with desktop publishing software
+                  like aldus pagemaker including versions of lorem ipsum.
+                </p>
+              </div>
+            </div>
+            <div class="bg-white overflow-hidden border border-gray-200 p-3">
+              <div class="m-2 text-justify text-sm font-qs">
+                <h2 class="font-bold text-lg h-2 mb-8">Capitalize sentence</h2>
+                <p class="text-sm">
+                  Lorem ipsum is simply dummy text of the printing and
+                  typesetting industry. lorem ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. it has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. it was popularised in the 1960s with
+                  the release of letraset sheets containing lorem ipsum
+                  passages, and more recently with desktop publishing software
+                  like aldus pagemaker including versions of lorem ipsum.
+                </p>
+              </div>
+            </div>
+            <div class="bg-white overflow-hidden border border-gray-200 p-3">
+              <div class="m-2 text-justify text-sm font-qs">
+                <h2 class="font-bold text-lg h-2 mb-8">Capitalize words</h2>
+                <p class="text-sm">
+                  Lorem Ipsum Is Simply Dummy Text Of The Printing And
+                  Typesetting Industry. Lorem Ipsum Has Been The Industry's
+                  Standard Dummy Text Ever Since The 1500s, When An Unknown
+                  Printer Took A Galley Of Type And Scrambled It To Make A Type
+                  Specimen Book. It Has Survived Not Only Five Centuries, But
+                  Also The Leap Into Electronic Typesetting, Remaining
+                  Essentially Unchanged. It Was Popularised In The 1960s With
+                  The Release Of Letraset Sheets Containing Lorem Ipsum
+                  Passages, And More Recently With Desktop Publishing Software
+                  Like Aldus Pagemaker Including Versions Of Lorem Ipsum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'About',
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,89 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center">
-    <div class="m-14">
-      <h1 class="font-qs text-gray-800 font-semibold text-5xl text-center">
-        Text Formatter
-      </h1>
-    </div>
-    <div>
-      <textarea
-        v-model="customText"
-        cols="120"
-        rows="10"
-        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none mb-4 font-qs"
-        placeholder="Your text..."
-      ></textarea>
-      <div class="flex justify-between">
-        <div class="flex gap-2">
-          <button
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
-            @click="ToUpperCaseAll"
-          >
-            Uppercase all
-          </button>
-          <button
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
-            @click="ToLowerCaseAll"
-          >
-            Lowercase all
-          </button>
-          <button
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
-            @click="ToCapitalizeSentence"
-          >
-            Capitalize sentence
-          </button>
-          <button
-            type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
-            @click="ToCapitalizeWords"
-          >
-            Capitalize words
-          </button>
-        </div>
-        <div class="">
-          <button
-            type="button"
-            class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg font-medium px-5 py-2.5 text-center font-qs flex justify-center items-center gap-1 text-sm"
-            @click="CopyToClipboard"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="1"
+  <div class="">
+    <div class="h-screen flex flex-col justify-center items-center">
+      <div class="m-14">
+        <h1 class="font-qs text-gray-800 font-semibold text-5xl text-center">
+          Text Formatter
+        </h1>
+      </div>
+      <div>
+        <textarea
+          v-model="customText"
+          cols="120"
+          rows="10"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none mb-4 font-qs"
+          placeholder="Your text..."
+        ></textarea>
+        <div class="flex justify-between">
+          <div class="flex gap-2">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
+              @click="ToUpperCaseAll"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-              />
-            </svg>
-            <span>Copy to clipboard</span>
-          </button>
+              Uppercase all
+            </button>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
+              @click="ToLowerCaseAll"
+            >
+              Lowercase all
+            </button>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
+              @click="ToCapitalizeSentence"
+            >
+              Capitalize sentence
+            </button>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 font-qs text-sm"
+              @click="ToCapitalizeWords"
+            >
+              Capitalize words
+            </button>
+          </div>
+          <div class="">
+            <button
+              type="button"
+              class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg font-medium px-5 py-2.5 text-center font-qs flex justify-center items-center gap-1 text-sm"
+              @click="CopyToClipboard"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+              <span>Copy to clipboard</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
+    <About />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import About from '~/components/About.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: {
+    About,
+  },
 
   data() {
     return {
@@ -86,21 +93,22 @@ export default Vue.extend({
 
   methods: {
     ToUpperCaseAll() {
-      this.customText = this.customText.toUpperCase()
+      this.customText = this.customText.trim().toUpperCase()
     },
 
     ToLowerCaseAll() {
-      this.customText = this.customText.toLowerCase()
+      this.customText = this.customText.trim().toLowerCase()
     },
 
     ToCapitalizeSentence() {
       this.customText =
-        this.customText.charAt(0).toUpperCase() +
-        this.customText.slice(1).toLowerCase()
+        this.customText.trim().charAt(0).toUpperCase() +
+        this.customText.trim().slice(1).toLowerCase()
     },
 
     ToCapitalizeWords() {
       this.customText = this.customText
+        .trim()
         .split(' ')
         .map((word) => {
           return word[0].toUpperCase() + word.substring(1).toLowerCase()
